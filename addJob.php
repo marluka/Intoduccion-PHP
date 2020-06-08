b7c08d3a4929e535d2cc7b988ea4aa0bc4c36982
Marly Mejia <marly.mejia@gmail.com>
2020-06-09 00:39:23 +0200

    Clase 27: Formularios

---
+++ addJob.php
@@ -0,0 +1,33 @@
+<?php
+  /* variables super globales */
+  var_dump($_GET);
+  var_dump($_POST);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
+    integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous" />
+  <link rel="stylesheet" href="style.css" />
+  <title>Formulario</title>
+</head>
+
+<body>
+  <form action="addJob.php" method="post">
+    <h1>Add Job</h1>
+    <label for="title">Title:
+      <input type="text" name="title" />
+    </label>
+    <label for="description">Descripción:
+      <input type="text" name="description" />
+    </label>
+    <button type="submit">Enviar</button>
+  </form>
+</body>
+
+</html>
