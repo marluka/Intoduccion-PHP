65dd5f475eb1174d7e6d26fbe824144b4bb733ee
Marly Mejia <marly.mejia@gmail.com>
2020-06-14 16:48:09 +0200

    Clase 38: Twig

--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Models\Project;
 
-class ProjectsController {
+class ProjectsController extends BaseController {
   public function getAddProjectAction($request) {
 
     if ($request->getMethod()=='POST') {
@@ -15,7 +15,7 @@ class ProjectsController {
       $project->save();
     }
 
-    include('../views/addProject.php');
+    echo $this->renderHTML('addProject.twig');
   }
 
 
