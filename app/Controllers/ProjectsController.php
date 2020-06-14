bd7a26f9fd1f1a78361d12c3725f32d18a2bf063
Marly Mejia <marly.mejia@gmail.com>
2020-06-14 22:56:43 +0200

    Clase 41: Validaciones

--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -3,19 +3,34 @@
 namespace App\Controllers;
 
 use App\Models\Project;
+use Respect\Validation\Validator as v;
 
 class ProjectsController extends BaseController {
   public function getAddProjectAction($request) {
+    $responseMessage = null;
 
     if ($request->getMethod()=='POST') {
       $postData = $request->getParsedBody();
-      $project = new Project();
-      $project->title = $postData['title'];
-      $project->description = $postData['description'];
-      $project->save();
+      $projectValidator = v::key('title', v::stringType()->notEmpty())
+                      ->key('description', v::stringType()->notEmpty());
+
+      try {
+        $projectValidator->assert($postData);
+        $postData = $request->getParsedBody();
+        $project = new Project();
+        $project->title = $postData['title'];
+        $project->description = $postData['description'];
+        $project->save();
+
+        $responseMessage = 'Saved';
+        
+      } catch (\Exception $e) {
+        $responseMessage = $e->getMessage();
+      }                
+      
     }
 
-    return $this->renderHTML('addProject.twig');
+    return $this->renderHTML('addProject.twig', ['responseMessage' => $responseMessage]);
   }
 
 
