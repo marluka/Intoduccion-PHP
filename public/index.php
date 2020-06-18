af7ec0c2c34571140ffb42e2055657adfdfb8183
Marly Mejia <marly.mejia@gmail.com>
2020-06-18 22:16:29 +0200

    Clase 44: Reto. Creación de usuarios

--- public/index.php
+++ public/index.php
@@ -42,6 +42,7 @@ $map->get('index', '/09-INTRODUCCION-PHP/Repositorio/', [
   'controller' => 'App\Controllers\IndexController',
   'action' => 'indexAction'
 ]);
+
 $map->get('addJob', '/09-INTRODUCCION-PHP/Repositorio/jobs/add', [
   'controller' => 'App\Controllers\JobsController',
   'action' => 'getAddJobAction'
@@ -50,6 +51,7 @@ $map->post('saveJob', '/09-INTRODUCCION-PHP/Repositorio/jobs/add', [
   'controller' => 'App\Controllers\JobsController',
   'action' => 'getAddJobAction'
 ]);
+
 $map->get('addProject', '/09-INTRODUCCION-PHP/Repositorio/projects/add', [
   'controller' => 'App\Controllers\ProjectsController',
   'action' => 'getAddProjectAction'
@@ -59,46 +61,55 @@ $map->post('saveProject', '/09-INTRODUCCION-PHP/Repositorio/projects/add', [
   'action' => 'getAddProjectAction'
 ]);
 
+$map->get('addUser', '/09-INTRODUCCION-PHP/Repositorio/users/add', [
+  'controller' => 'App\Controllers\UsersController',
+  'action' => 'getAddUserAction'
+]);
+$map->post('saveUser', '/09-INTRODUCCION-PHP/Repositorio/users/add', [
+  'controller' => 'App\Controllers\UsersController',
+  'action' => 'getAddUserAction'
+]);
+
 $matcher = $routerContainer->getMatcher();
 $route = $matcher->match($request);
 
-function printJob($job){
+// function printJob($job){
 
-  // if ($job->visible == false) {
-  //   return;
-  // }
+//   // if ($job->visible == false) {
+//   //   return;
+//   // }
 
-  echo '<li class="work-position">';
-  echo    '<h5>'.$job->title.'</h5>';
-  echo    '<p>'.$job->description.'</p>';
-  echo    '<p>'.$job->getDurationAsString().'</p>';
-  echo    '<strong>Achievements:</strong>';
-  echo    '<ul>';
-  echo      '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo      '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo      '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo    '</ul>';
-  echo '</li>';
-}
+//   echo '<li class="work-position">';
+//   echo    '<h5>'.$job->title.'</h5>';
+//   echo    '<p>'.$job->description.'</p>';
+//   echo    '<p>'.$job->getDurationAsString().'</p>';
+//   echo    '<strong>Achievements:</strong>';
+//   echo    '<ul>';
+//   echo      '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+//   echo      '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+//   echo      '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+//   echo    '</ul>';
+//   echo '</li>';
+// }
 
-function printProject($project){
+// function printProject($project){
 
-  echo '<div class="project">';
-  echo '  <h5>'.$project->title.'</h5>';
-  echo '  <div class="row">';
-  echo '    <div class="col-3">';
-  echo '      <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">';
-  echo '    </div>';
-  echo '    <div class="col">';
-  echo '      <p>'.$project->description.'</p>';
-  echo '      <strong>Technologies used:</strong>';
-  echo '      <span class="badge badge-secondary">PHP</span>';
-  echo '      <span class="badge badge-secondary">HTML</span>';
-  echo '      <span class="badge badge-secondary">CSS</span>';
-  echo '    </div>';
-  echo '  </div>';
-  echo '</div>';
-}
+//   echo '<div class="project">';
+//   echo '  <h5>'.$project->title.'</h5>';
+//   echo '  <div class="row">';
+//   echo '    <div class="col-3">';
+//   echo '      <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">';
+//   echo '    </div>';
+//   echo '    <div class="col">';
+//   echo '      <p>'.$project->description.'</p>';
+//   echo '      <strong>Technologies used:</strong>';
+//   echo '      <span class="badge badge-secondary">PHP</span>';
+//   echo '      <span class="badge badge-secondary">HTML</span>';
+//   echo '      <span class="badge badge-secondary">CSS</span>';
+//   echo '    </div>';
+//   echo '  </div>';
+//   echo '</div>';
+// }
 
 if (!$route) {
   echo 'No route';
