417d989aec6d60d70c6a426f9d79b890a59cfcb6
Marly Mejia <marly.mejia@gmail.com>
2020-06-20 12:18:02 +0200

    Renombre de la función Dotenv::createImmutable para poder usar el método getenv()

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ require_once '../vendor/autoload.php';
 
 session_start();
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__.'/..');
 $dotenv->load();
 
 use Illuminate\Database\Capsule\Manager as Capsule;
@@ -18,10 +18,10 @@ $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => $_ENV['DB_HOST'],
-    'database'  => $_ENV['DB_NAME'],
-    'username'  => $_ENV['DB_USER'],
-    'password'  => $_ENV['DB_PASS'],
+    'host'      => getenv('DB_HOST'),
+    'database'  => getenv('DB_NAME'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASS'),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
